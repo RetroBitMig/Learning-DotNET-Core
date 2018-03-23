cae2e39a7bc07f44fc97fdf3eb41baef7b9a1d9b
Mig <RetroBitMig@noreply.github.com>
2018-03-22 18:19:57 -0700

    Part 2 - Loading (local) images

--- SimpleImageGallery/Controllers/GalleryController.cs
+++ SimpleImageGallery/Controllers/GalleryController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SimpleImageGallery.Data.Models;
 using SimpleImageGallery.Models;
 
 namespace SimpleImageGallery.Controllers
@@ -11,9 +12,60 @@ namespace SimpleImageGallery.Controllers
     {
         public IActionResult Index()
         {
+            var hikingImageTags = new List<ImageTag>();
+            var cityImageTags = new List<ImageTag>();
+
+            var tag1 = new ImageTag()
+            {
+                Description = "Adventure",
+                Id = 0
+            };
+
+            var tag2 = new ImageTag()
+            {
+                Description = "Urban",
+                Id = 1
+            };
+
+            var tag3 = new ImageTag()
+            {
+                Description = "New York",
+                Id = 2
+            };
+
+            hikingImageTags.Add(tag1);
+
+            cityImageTags.AddRange(new List<ImageTag> { tag2, tag3 });
+
+            var imageList = new List<GalleryImage>()
+            {
+                new GalleryImage()
+                {
+                    Title = "Hiking Trip",
+                    Url = "https://static.pexels.com/photos/16714/pexels-photo.jpg",
+                    Created = DateTime.Now,
+                    Tags = hikingImageTags
+                },
+                new GalleryImage()
+                {
+                    Title = "On the Trail",
+                    Url = "https://static.pexels.com/photos/314703/pexels-photo-314703.jpeg",
+                    Created = DateTime.Now,
+                    Tags = hikingImageTags
+                },
+                new GalleryImage()
+                {
+                    Title = "Downtown",
+                    Url = "https://static.pexels.com/photos/378570/pexels-photo-378570.jpeg",
+                    Created = DateTime.Now,
+                    Tags = cityImageTags
+                }
+            };
+
             var model = new GalleryIndexModel()
             {
-
+                Images = imageList,
+                SearchQuery = ""
             };
 
             return View(model);
